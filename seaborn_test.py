3fbba64c504153e6e12a8316a98a1ddcc479ade0
Jittsu <naoto.j3781@gmail.com>
2018-11-26 18:03:56 +0900

    test完了

--- seaborn_test.py
+++ seaborn_test.py
@@ -15,10 +15,20 @@ def main():
     sns.set()
     f = sys.argv[1]
     df = pd.read_csv(f)
-    df = df[["length","fav_novel_cnt","all_point","general_all_no","review_cnt","sasie_cnt","kaiwaritu"]]
-    print(df)
+    df = df[["fav_novel_cnt","length","all_point","general_all_no","review_cnt","kaiwaritu"]]
+    #df.length = df.length.apply(lambda x:int(x.replace(",","")))
+    #df.fav_novel_cnt = df.fav_novel_cnt.apply(lambda x:int(x.replace(",","")))
+    #df.all_point = df.all_point.apply(lambda x:int(x.replace(",","")))
+    #df = df[df.fav_novel_cnt > 100]
+    #print(df.head())
+    #print(df.tail())
+    #print(df)
     sns.pairplot(df)
+    #graph = sns.pairplot(df)
     #sns.jointplot("sasie_cnt","kaiwaritu", data=df)
+    #plt.xscale("log")
+    #plt.yscale("log")
+    #graph.set(xscale="log", yscale="log")
     plt.show()
     
 
