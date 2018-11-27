404f17120df8f51debdcd6b928932afc940098b8
Jittsu <naoto.j3781@gmail.com>
2018-11-27 15:09:14 +0900

    change the code to use tfidf_to_pkl.py

--- narou_tfidf.py
+++ narou_tfidf.py
@@ -7,36 +7,30 @@ this is a code of culculating TF-IDF value of story
 
 import os
 import sys
-#import glob
 import MeCab
 import csv
-import pickle
-#import pandas as pd
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
+from tfidf_to_pkl import tfidf_transformer as tt
+from tfidf_to_pkl import pickle_transformer as pt
+
 
 # 形態素解析 story => ['文書1の分かち書き(空白区切り)','文書2',...,'文書n']
-def wakati():
+def wakati(f):
     mecab = MeCab.Tagger("wakati")
     mecab.parse('') # エラー避け ---   
-    f = sys.argv[1]
     f = csv.reader(open(f, "r"))
     genre_bow = []
 
     # 1作品ごとにstoryをBoWに
     for line in f:
         story = ""
-        #print("---")
-        #print(line[31])
         words = mecab.parse(line[31])
-        #print(words)
         words = words.split("\n") # 分かち書きごとに分割 ---
 
         # 分かち書きした単語をリストに投入 ---
         for word in words:
-            #print(word)
             w1 = re.split(r"[\t,]", word)
-            #print(w1)
 
             if (w1[0] == "EOS") | (w1[0] == ""):
                 continue
@@ -45,26 +39,28 @@ def wakati():
                 if (w1[1] == "形容詞") | (w1[1] == "接続詞") | (w1[1] == "動詞") | (w1[1] == "名詞") | (w1[1] == "副詞"):
 
                     if w1[7] == "*":
-                        #print(w1[7])
-                        #print(w1)
-                        #w1[7] = w1[0]
-                        #print(w1[7])
                         continue # 数とかしかない ---
-                    #print(w1[7])
-                    #story.append(w1[7])
                     story = story + w1[7] + " " # 空白区切りのBoWを作る ---
-        #print(story)
         genre_bow.append(story)
-        #print(genre_bow)
     return genre_bow
 
 # tfidf値の計算 ---
 def tfidf(corpus): # 引数の形は['文書1の分かち書き(空白区切り)','文書2',...,'文書n'] ---
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(corpus)
-    print(tfidf_matrix)
+    print(tfidf_matrix.toarray())
+    
+    for k,v in sorted(vectorizer.vocabulary_.items(), key=lambda x:x[1]):
+        print(k, v)
 
 
 if __name__ == "__main__":
-    bow = wakati()
-    tfidf(bow)
+    fin = sys.argv[1]
+    out = sys.argv[2]
+    bow = wakati(fin)
+    #tfidf(bow)
+    tfidf = tt("word")
+    model, matrix = tfidf.tfidf_culc(bow)
+    #print(matrix.toarray())
+    save = pt(model)
+    save.save_pkl(out)
