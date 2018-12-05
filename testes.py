c043cdfbeedf4c8664d4fc36983dc9c6aa89f162
Jittsu <naoto.j3781@gmail.com>
2018-12-05 20:03:55 +0900

    change the code to test

--- testes.py
+++ testes.py
@@ -8,20 +8,3 @@ from matplotlib import pyplot as plt
 from elasticsearch import Elasticsearch, helpers
 es = Elasticsearch(timeout=180, max_retries=10, retry_on_timeout=True)
 
-print("select mode")
- 
-while(1):
-    mode = input(">> ")
-
-    if mode == "csv":
-        print("csv output")
-
-    elif mode == "pickle":
-        print("pickle output")
-
-    elif mode == "end":
-        print("end")
-        break
-
-    else:
-        continue
