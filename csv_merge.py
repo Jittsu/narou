4ff669c28ee0c6ef95bda80644b06643d3fabb00
Jittsu <naoto.j3781@gmail.com>
2018-12-05 20:02:18 +0900

    first commit

---
+++ csv_merge.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import sys
+import csv
+
+def main():
+    #l = []
+    f1 = sys.argv[1]
+    f2 = sys.argv[2]
+    n = sys.argv[3]
+
+    #l.append(pd.read_csv(f1, header=None))
+    #l.append(pd.read_csv(f2, header=None))
+    #df = pd.concat(l)
+
+    df1 = pd.read_csv(f1, header=None)
+    df2 = pd.read_csv(f2, header=None)
+
+    df = pd.concat([df1, df2], axis=1)
+
+    df.to_csv("/mnt/hdd1/data/narou/genre_reg/{0}_reg.csv".format(n), index=None, header=None)
+
+if __name__ == "__main__":
+    main()
