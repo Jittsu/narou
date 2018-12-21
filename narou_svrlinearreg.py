56e595902e7a8ebaa1596cd0eda0b8a3ec482492
Jittsu <naoto.j3781@gmail.com>
2018-12-21 21:21:50 +0900

    grid search regression

--- narou_svrlinearreg.py
+++ narou_svrlinearreg.py
@@ -16,8 +16,12 @@ from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 from math import sqrt
 from scipy import sparse
+from sklearn.model_selection import GridSearchCV
 
 def main():
+    tuned_parameters = {'C': [0.01,0.1,1,10,100,1000], 'kernel': ["rbf"], 'gamma': [0.01,0.001,0.0001,0.00001]}
+    clf = GridSearchCV(SVR(), tuned_parameters, cv=5, scoring="r2", n_jobs=10)
+    """
     f = sys.argv[1]
     df = pd.read_csv(f)
     X = df.iloc[:, 2:]
@@ -25,16 +29,37 @@ def main():
     Y = df.iloc[:, 1].values
     Y = np.log(Y+1).astype(np.float32)
     #print(X)
+    """
+    f = sys.argv[1]
+    df = pd.read_pickle(f)
+    X = df[:, 1:]
+    Y = df[:, 0].todense()
+    Y = pd.DataFrame(Y)
+    Y = Y.iloc[:, 0].values
+    Y = np.log(Y+1).astype(np.float32)
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=0)
 
-    svr_lin = SVR(kernel="rbf", C=20, gamma=1e-7)
-    y_lin = svr_lin.fit(X_train, Y_train).predict(X_train)
+    #svr_lin = SVR(kernel="rbf", C=10, gamma=1e-5)
+    #y_lin = svr_lin.fit(X_train, Y_train).predict(X_train)
+    clf.fit(X_train, Y_train)
+    Y_pred = clf.predict(X_test)
 
-    Y_pred = svr_lin.predict(X_test)
+    #Y_pred = svr_lin.predict(X_test)
 
     corr = np.corrcoef(Y_test, Y_pred)[0, 1]
-    #lin_RMS = sqrt(mean_squared_error(Y_test, test_lin))
+    RMSPE = (np.sqrt(np.mean(((Y_test-Y_pred) / (Y_test+1)) ** 2)))
+    print("Corr:%f, RMSPE:%f, R2_SCORE:%f\n%s" % (corr,RMSPE,clf.best_score_,clf.best_params_))
+
+    plt.title("Coor:%s, RMSPE:%s, R2_SCORE:%s, %s" % (corr,RMSPE,clf.best_score_,clf.best_params_))
+    plt.xlabel("Y_test")
+    plt.ylabel("Y_pred")
+    plt.scatter(Y_test, Y_pred)
+    plt.plot(Y_test, Y_test, color="r")
+    plt.show()
+
+    """
+    corr = np.corrcoef(Y_test, Y_pred)[0, 1]
     RMSPE = (np.sqrt(np.mean(((Y_test-Y_pred) / (Y_test+1)) ** 2)))
     R2_SCORE = r2_score(Y_test, Y_pred)
 
@@ -46,7 +71,7 @@ def main():
     plt.scatter(Y_test, Y_pred)
     plt.plot(Y_test, Y_test, color="r")
     plt.show()
-    
+    """
 
 
 if __name__ == "__main__":
