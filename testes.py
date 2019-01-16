f703e4ef18ed52bc6bd2cb016374249c6fe4ef92
Jittsu <naoto.j3781@gmail.com>
2019-01-16 21:36:13 +0900

    do test

--- testes.py
+++ testes.py
@@ -1,20 +1,6 @@
-import os
-import glob
-import sys
-import json
-import pandas as pd
-import numpy as np
-import seaborn as sns
-from matplotlib import pyplot as plt
-from elasticsearch import Elasticsearch, helpers
-from sklearn.preprocessing import scale
+import time
 
-es = Elasticsearch(timeout=180, max_retries=10, retry_on_timeout=True)
+time.sleep(3)
+print("a")
 
-df = pd.DataFrame(data=[[1,1,1],[1,0.5,1],[1,0,1]])
-df1 = df.iloc[:,1]
-df2 = df1.values
-df3 = scale(df2)
-df4 = pd.DataFrame(df3)
-df[1] = df4
-print(df)
+#print(a)
